960cc16ad4c2c94a06097702ee0816f9b48ed3b9
Lukasz Sajna <lukasz.sajna@gmail.com>
2023-09-06 02:14:32 +0200

    Regular auto-commit at 02:14:32 on 06/09/2023

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd /c/Program Files \(x86\)/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/ 
+cd /c/Program\ Files \(x86\)/Steam/steamapps/common/Counter-Strike\ Global\ Offensive/ 
 git add --all
 timestamp() {
   date +"at %H:%M:%S on %d/%m/%Y"
