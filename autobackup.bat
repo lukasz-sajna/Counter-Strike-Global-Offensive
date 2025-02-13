7309e3a740060a3459fd96c6b4098ee513036bf6
Lukasz Sajna <lukasz.sajna@gmail.com>
2025-02-13 03:05:31 +0100

    Regular auto-commit at 03:05:31 on  1/zw/.02.

--- autobackup.bat
+++ autobackup.bat
@@ -2,11 +2,13 @@
 cd "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive"
 cmd /c (
   git add .
+
+  :: Pobranie poprawnego formatu godziny
   set time=%TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
   set time=%time: =0%
 
-  set date=%DATE:~0,2%/%DATE:~3,2%/%DATE:~6,4%
-  set date=%date: =0%
+  :: Pobranie poprawnego formatu daty (z systemu polskiego)
+  set date=%DATE:~4,2%/%DATE:~1,2%/%DATE:~7,4%
 
   git commit -am "Regular auto-commit at %time% on %date%"
   git push origin master
